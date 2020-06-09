aa3055a08498f7a333d2a866f41cebe77f041a6e
HarukaGame <harukagamecreate@gmail.com>
2020-06-09 23:41:45 +0900

    シェーダーの追加

---
+++ OpenglProject/OpenglOrpject/game/ShaderFiles/basic.fs
@@ -0,0 +1,10 @@
+#shader fragment
+#version 120
+uniform vec4 color;
+uniform vec3 light;
+varying vec3 _normals;
+void main(void) {
+    //float d = clamp(dot(light,vec3(1,1,1)),0.0,1.0) + 1.0;
+    float d = clamp(dot(light,_normals),0,1) + 0.2;
+    gl_FragColor = d* color;
+}
