9c678a2c32a96c3e3ef6ffa67e4e82cfbc6f7088
HarukaGame <harukagamecreate@gmail.com>
2020-06-09 22:21:34 +0900

    fileloaderの作成

--- OpenglProject/OpenglOrpject/game/fileloader.cpp
+++ OpenglProject/OpenglOrpject/game/fileloader.cpp
@@ -2,6 +2,51 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#  pragma warning(disable: 4996)
+bool CFileLoader::LoadFile(const char* _filePath)
+{
+    FILE* fp;
+
+    fp = fopen(_filePath, "rb");
+    if (fp == nullptr) {
+        printf("fopen error\n");
+        return false;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    m_length = ftell(fp);
+
+    m_pBuffer = malloc(m_length);
+    if (m_pBuffer == nullptr) {
+        return false;
+    }
+    fseek(fp, 0L, SEEK_SET);
+
+    bool success = fread(m_pBuffer, 1, m_length, fp) != (size_t)m_length;
+    fclose(fp);
+
+    if (success) {
+        return false;
+    }
+
+    return true;
+
+}
+
+const char* CFileLoader::GetBuffer()
+{
+    return (char*)m_pBuffer;
+}
+
+int CFileLoader::GetLength()
+{
+    return m_length;
+}
+
+void CFileLoader::Release()
+{
+    free(m_pBuffer);
+}
 
 const char* CFileLoader::GetText(const char* filename, const char* code) {
     //ファイルの読み込み
--- OpenglProject/OpenglOrpject/game/fileloader.h
+++ OpenglProject/OpenglOrpject/game/fileloader.h
@@ -3,5 +3,12 @@
 
 class CFileLoader {
 public:
+    bool LoadFile(const char* _filePath);
+    const char* GetBuffer();
+    int GetLength();
+    void Release();
     static const char* GetText(const char* filename, const char* code);
+private:
+    void* m_pBuffer = nullptr;
+    int m_length = 0;
 };
