25eb792c960a1de0ea5b30d1d61af86c9815402b
HarukaGame <harukagamecreate@gmail.com>
2020-06-13 23:25:07 +0900

    メモリリーク検出の追加

--- OpenglProject/OpenglOrpject/game/main.cpp
+++ OpenglProject/OpenglOrpject/game/main.cpp
@@ -1,11 +1,13 @@
 ﻿#include "application.h"
-
+#include "crtdbg.h"
 
 int WINAPI WinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR lpCmdLine,
     int nCmdShow)
 {
+    // メモリリーク検出
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     CApplication app;
     app.Initilize(hInstance,hPrevInstance,lpCmdLine,nCmdShow);
 
